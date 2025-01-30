8d4c02eaa99e85a44e097519d65234939c5d69c6
Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>
2025-01-30 15:41:50 +0100

    revert changes on flodym arrays

--- flodym/flodym_arrays.py
+++ flodym/flodym_arrays.py
@@ -317,11 +317,6 @@ class FlodymArray(PydanticBaseModel):
     def __truediv__(self, other):
         other = self._prepare_other(other)
         dims_out = self.dims.union_with(other.dims)
-        if np.any(other.values == 0):
-            raise ZeroDivisionError(
-                f"Division by zero should not occur - change {self.__class__} "
-                f"to define how this should be handled."
-            )
         values_out = np.einsum(
             f"{self.dims.string},{other.dims.string}->{dims_out.string}",
             self.values,
@@ -367,11 +362,6 @@ class FlodymArray(PydanticBaseModel):
         return self * other
 
     def __rtruediv__(self, other):
-        if np.any(self.values == 0):
-            raise ZeroDivisionError(
-                f"Division by zero should not occur - change {self.__class__} "
-                f"to define how this should be handled."
-            )
         inv_self = FlodymArray(dims=self.dims, values=1 / self.values)
         return inv_self * other
 
@@ -454,12 +444,7 @@ class FlodymArray(PydanticBaseModel):
         if all([d in dim_letters for d in self.dims.letters]):
             return self / self.sum_values()
 
-        sum_self = self.sum_over(sum_over_dims=dim_letters)
-        sum_self = sum_self.maximum(
-            1e-10
-        )  # avoid division by zero for shares, result will be zero anyways
-
-        return self / sum_self
+        return self / self.sum_over(sum_over_dims=dim_letters)
 
 
 class SubArrayHandler:
